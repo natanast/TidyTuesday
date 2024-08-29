8447cdf4bb9e03d6644559be6027fce0bd6db34e
NatAnastas <anastasiadounat@gmail.com>
2024-08-29 16:31:10 +0300

    29082024

--- R/2024-08-26/2024-08-26.R
+++ R/2024-08-26/2024-08-26.R
@@ -13,6 +13,25 @@ df <-  power_rangers_seasons |> as.data.table()
 
 df$season_num <- df$season_num |> as.character()
 
+df <- df[order(IMDB_rating)]
+
+# Create a new column for categorizing top 5 and bottom 5
+df[, category := "Other"]
+df[1:5, category := "Top 5"]
+df[(.N-4):.N, category := "Bottom 5"]
+
+
+
+# df$x <- paste0(df$season_title, " (Season ", df$season_num, ")")
+
+# Create a combined column that adds season number only if not already in the title
+df$x <- ifelse(grepl("\\(Season \\d+\\)", df$season_title),
+               df$season_title,
+               paste0(df$season_title, " (Season ", df$season_num, ")"))
+
+
+df$x <- factor(df$x, levels = df$x)
+
 
 
 # Plot 1---------------
@@ -22,21 +41,34 @@ colors <- c("#6A6599", "#BC3C29", "#DF8F44", "#79AF97", "#0072B5")
 
 # Plot
 library(ggplot2)
-
+library(extrafont)
+library(ggtext)
 
 # Plot
-ggplot(df, aes(x = season_num, y = IMDB_rating)) +
+ggplot(df, aes(x = x, y = IMDB_rating, fill = category)) +
   
-  geom_bar(stat = "identity", alpha = 0.8, width = 0.7) +
+  geom_bar(stat = "identity", alpha = 0.65, width = 0.7) +
+  
+  geom_text(aes(label = IMDB_rating), 
+            hjust = -0.1,  # Positioning the text just outside the bar
+            color = "black", 
+            size = 3.5) +
   
   coord_flip() +
   
-  #scale_fill_manual(values = colors) +
+  scale_fill_manual(values = c("Top 5" = "#0072B5", "Bottom 5" = "#BC3C29", "Other" = "gray50")) +
   
   labs(
-    title = "Top 5 Parent Entities by Total CO2 Emissions",
-    x = "Parent Entity",
-    y = "Total MtCO2"
+    title = "IMDB rating of the Power Rangers seasons",
+    subtitle = "IMDB rating of the Power Rangers seasons",
+    
+    x = "Season",
+    y = "IMDB rating",
+    
+    caption = paste0(
+      "Source: <b>NationalDayCalendar.com</b> | ",
+      "Graphic: <b>Natasa Anastasiadou</b>"
+    )
   ) +
   
   theme_minimal() +
@@ -44,14 +76,13 @@ ggplot(df, aes(x = season_num, y = IMDB_rating)) +
   theme(
     legend.position = "none",
     
-    #axis.line = element_line(linewidth = 0.55),
-    #axis.ticks = element_line(linewidth = 0.55),
-    
-    plot.title = element_text(hjust = 0.5),
+    plot.title = element_text(size = 20, face = "bold", hjust = 0.5, family = "Candara"),
+    plot.subtitle = element_text(size = 14, hjust = 0.5, family = "Candara", color = "grey30"),
+    plot.caption  = element_markdown(margin = margin(t = 25), size = 8, family = "Candara", hjust = 1),
     
     plot.margin = margin(20, 20, 20, 20),
     
-    plot.background = element_rect(fill = "#e4e4e3", color = NA)
+    plot.background = element_rect(fill = "grey94", color = NA)
   )
 
 
