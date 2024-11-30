16e98df41b7d43b1602f0b16358c3e493e2a2700
NatAnastas <anastasiadounat@gmail.com>
2024-11-30 15:17:01 +0200

    new_plot

--- R/2024-11-26/2024-11-26.R
+++ R/2024-11-26/2024-11-26.R
@@ -8,114 +8,105 @@ gc()
 
 library(data.table)
 library(ggplot2)
-# library(ggtext)
-# library(extrafont)
-# library(ggthemes)
-# library(paletteer)
-# library(colorspace)
-# library(ggrepel)
+library(sf)
+library(usmap)
+library(extrafont)
+library(ggtext)
+library(tidyverse)
+library(shadowtext)
 
 
 # load data --------
 
-cbp_resp <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-11-26/cbp_resp.csv')
+# cbp_resp <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-11-26/cbp_resp.csv')
 cbp_state <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-11-26/cbp_state.csv')
 
 
 # clean data -------
-library(tidyverse)
 
-library(tidyverse)
+# Prepare map
+map <- us_map() |> 
+    select(state = abbr) |> 
+    st_as_sf()
 
-cbp_resp %>%
-    group_by(month_abbv) %>%
-    summarize(total_encounters = sum(encounter_count, na.rm = TRUE)) %>%
-    ggplot(aes(x = month_abbv, y = total_encounters)) +
-    geom_line(group = 1, color = "green") +
-    geom_point(color = "blue") +
-    labs(title = "Seasonal Trends in Encounters", 
-         x = "Month", 
-         y = "Total Encounters") +
-    theme_minimal() +
-    scale_x_discrete(limits = c("JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"))
+# Filter and prepare data
+data <- cbp_state[, .(fiscal_year, state, encounter_count)]
 
-# plot --------
-# 
-# gr = ggplot(d, aes(x = episode, y = season, group = season)) +
-#     
-#     geom_line(color = "grey65", size = 0.6, alpha = 0.7) +
-#     
-#     geom_point(
-#         aes(fill = unique_words),
-#         size = 3.5,
-#         shape = 21,
-#         stroke = 0.15,
-#         alpha = 0.9
-#     ) +
-#     
-#     scale_fill_stepsn(
-#         colors = c("#7d7ca9","#abaad9","#ffffe0","#ff9a92","#b24745"),
-#         breaks = c(1000, 1200, 1400),
-#         guide = guide_colorsteps(
-#             title = "Bubble Color",
-#             barheight = unit(7, "lines"),
-#             barwidth = unit(0.5, "lines")
-#             
-#         )
-#     ) +
-# 
-#     coord_radial(inner.radius = .2) +
-#     
-#     theme_minimal() +
-#     
-#     # Adjust x-axis limits to start from 1
-#     scale_x_continuous(limits = c(1, max(d$episode) + 2), expand = c(0, 0)) +
-#     
-#     geom_text(aes(label = season_text),
-#               na.rm = TRUE,
-#               color = "grey50",
-#               size = 2.5,
-#               fontface = "bold",
-#               hjust = 1.75,
-#               vjust = 0.35) +
-#     
-#     labs(
-#         title = "Radial plot of Unique Words per episode across 14 seasons.",
-#         subtitle = "Each bubble represents an <b>Episode</b>. Each circle represents a <b>Season</b>. <br> Episodes with unique words ranging from <b><span style='color: #7d7ca9; font-weight: bold;'>1000 to 1200</span></b> appear more often.",
-#         caption = "Source: <b>  {bobsburgersR} R Package</b> | Graphic: <b>Natasa Anastasiadou</b>"
-#     ) +
-#     
-#     theme(
-# 
-#         legend.position = "right",
-#         legend.title.position = "left",
-#         
-#         legend.title = element_text(size = 7, face = "bold", family = "Candara", color = "grey30", angle = 90, hjust = .5),
-#         legend.text = element_text(size = 7, family = "Candara", color = "grey30"),
-# 
-#         axis.title.y = element_blank(),
-#         axis.title.x = element_blank(),
-#         axis.text.x = element_blank(), 
-#         axis.text.y = element_blank(), 
-#         
-#         panel.grid.major = element_line(linewidth = .35, color = "grey85"),
-#         panel.grid.minor = element_blank(),
-#         
-#         plot.title = element_text(size = 15, face = "bold", hjust = 0.5, vjust = -15, family = "Candara" ),
-#         plot.subtitle = element_markdown(size = 11, hjust = 0.5, family = "Candara", color = "grey30", margin = margin(60)),
-#         plot.caption = element_markdown(margin = margin(t = 12), size = 6, family = "Candara", hjust = 1.27),
-#         
-#         plot.margin = margin(5, 5, 5, 5),
-#         
-#         plot.background = element_rect(fill = "#e4e4e3", color = NA)
-#     )
-# gr
-# 
-# ggsave(
-#     plot = gr, filename = "Rplot.png",
-#     width = 8, height = 8, units = "in", dpi = 600
-# )    
-# 
-# 
-# 
-# 
+data_agg <- data[, .(encounter_count = sum(encounter_count, na.rm = TRUE)), by = state]
+
+# Merge data with map
+map_data <- map |> 
+    left_join(data_agg, by = c("state" = "state"))
+
+
+# Calculate centroids for state labels
+centroids <- st_centroid(map_data)
+centroids_coords <- st_coordinates(centroids)
+map_data$centroid_x <- centroids_coords[, 1]
+map_data$centroid_y <- centroids_coords[, 2]
+
+
+
+# plot -------
+
+gr = ggplot(map_data) +
+    
+    geom_sf(data = map, fill = "grey95", linewidth = 0.05) +
+    
+    geom_sf(aes(fill = encounter_count), color = "gray25", linewidth = 0.2) +  # Fill based on encounters
+    
+    # Add text with a shadow (stroke effect)
+    geom_shadowtext(
+        aes(x = centroid_x, y = centroid_y, label = state),
+        size = 3.5,                    
+        family = "Candara",            
+        color = "gray30",               
+        bg.color = "white",            
+        bg.r = 0.06                     
+    ) +
+    
+    scale_fill_stepsn(
+        colors = c("#7d7ca9","#abaad9","#ffffe0","#ff9a92","#b24745"),
+        na.value = "grey95",
+        labels = scales::comma,
+        guide = guide_colorsteps(
+            title = "Encounters",
+            barheight = unit(0.7, "lines"),
+            barwidth = unit(16, "lines")
+            
+        )
+    ) +
+    
+    labs(
+        title = "State-by-State Customs and Border Protection (CBP) Encounters",
+        subtitle = "A comprehensive view of encounters reported by U.S. CBP for each state.",
+        caption = "Source: <b> U.S. Customs and Border Protection (CBP) Encounter Data</b> | Graphic: <b>Natasa Anastasiadou</b>"
+    ) +
+    
+    theme_void() +  
+    
+    theme(
+        
+        legend.position = "bottom",
+        legend.title.position = "top",
+        
+        legend.title = element_text(size = 9, face = "bold", family = "Candara", color = "grey30", hjust = .5),
+        legend.text = element_text(size = 7, family = "Candara", color = "grey30"),
+        
+
+        plot.title = element_markdown(size = 20, face = "bold", hjust = 0.5, family = "Candara", margin = margin(t = 25, l = 50)),
+        plot.subtitle = element_markdown(size = 15, hjust = 0.5, family = "Candara", color = "grey30", margin = margin(t = 10, l = 50)),
+        plot.caption = element_markdown(margin = margin(t = 35), size = 8, family = "Candara", hjust = 1.03),
+        
+        plot.margin = margin(20, 20, 20, 20),
+        
+        plot.background = element_rect(fill = "grey93", color = NA)
+    )
+
+gr
+
+ggsave(
+    plot = gr, filename = "Rplot.png",
+    width = 10, height = 9, units = "in", dpi = 600
+)
+
