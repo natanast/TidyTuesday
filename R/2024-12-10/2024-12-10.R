ce02057d200ac9265d2db5a33fb590bd4c29dad4
Natasa Anastasiadou <141711668+natanast@users.noreply.github.com>
2024-12-11 18:37:34 +0200

    Update 2024-12-10.R

--- R/2024-12-10/2024-12-10.R
+++ R/2024-12-10/2024-12-10.R
@@ -15,3 +15,4 @@ library(ggtext)
 
 # load data --------
 
+parfumo_data_clean <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2024/2024-12-10/parfumo_data_clean.csv')
