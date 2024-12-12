b7fc17d3fbdf0866b6066f7a80b5f728091ff997
NatAnastas <anastasiadounat@gmail.com>
2024-12-12 21:40:46 +0200

    data cleaning

--- R/2024-12-10/2024-12-10.R
+++ R/2024-12-10/2024-12-10.R
@@ -16,3 +16,18 @@ library(ggtext)
 # load data --------
 
 parfumo_data_clean <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2024/2024-12-10/parfumo_data_clean.csv')
+
+
+# data cleaning -----
+
+d = parfumo_data_clean[, c("Name", "Brand", "Release_Year", "Rating_Value", "Rating_Count", "Perfumers" )]
+
+
+d <- d[!is.na(d$Release_Year) & !is.na(d$Rating_Value) & !is.na(d$Rating_Count), ]
+
+
+
+
+
+
+
