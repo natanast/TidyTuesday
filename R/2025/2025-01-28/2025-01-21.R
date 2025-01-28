2bcf4393c88d66de4d70b88262212c627ae542fd
NatAnastas <anastasiadounat@gmail.com>
2025-01-28 15:13:25 +0200

    dataset

--- R/2025/2025-01-28/2025-01-21.R
+++ R/2025/2025-01-28/2025-01-21.R
@@ -14,6 +14,8 @@ library(extrafont)
 
 # load data --------
 
+water_insecurity_2022 <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-01-28/water_insecurity_2022.csv')
+water_insecurity_2023 <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-01-28/water_insecurity_2023.csv')
 
 
 # data cleaning ------
