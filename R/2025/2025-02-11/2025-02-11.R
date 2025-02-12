04a3765e09d2240ac96799e301ddb65701d380c1
NatAnastas <anastasiadounat@gmail.com>
2025-02-12 17:01:37 +0200

    20250212

--- R/2025/2025-02-11/2025-02-11.R
+++ R/2025/2025-02-11/2025-02-11.R
@@ -16,14 +16,13 @@ library(ggtext)
 
 # load data --------
 
-
 cdc_datasets <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-02-11/cdc_datasets.csv')
 fpi_codes <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-02-11/fpi_codes.csv')
 omb_codes <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-02-11/omb_codes.csv')
 
 # data cleaning ------
 
-
+cdc_datasets$category |> unique() |> length()
 # ggsave(
 #     plot = gr, filename = "Rplot.png",
 #     width = 10, height = 10, units = "in", dpi = 600
