2c68999ca29aaf29fa364391c766100bad7c70df
NatAnastas <anastasiadounat@gmail.com>
2025-02-15 13:20:06 +0200

    changes

--- R/2025/2025-02-11/2025-02-11.R
+++ R/2025/2025-02-11/2025-02-11.R
@@ -27,6 +27,13 @@ col = c('#60608b', '#6c6c98', '#9291be', '#9e9ecb', '#b9b8e5',
         '#dc756e', '#d0645f', '#c15451')
 
 
+
+
+# Adding '*' to the categories that need explanation
+df$category <- ifelse(df$category == "NNDSS", "*NNDSS", 
+                      ifelse(df$category == "NCHS", "**NCHS", df$category))
+
+
 # Apply word wrap to category
 df$category <- str_wrap(df$category, width = 30)
 
@@ -45,7 +52,7 @@ gr = ggplot(data = df) +
         ),
         shape = 21, stroke = .25, alpha = 0.8
     ) +
-    scale_size_continuous(range = c(7, 15)) +
+    scale_size_continuous(range = c(6, 13)) +
     scale_fill_manual(values = col) +  # Use the reversed ordered color palette
     theme_minimal() +
     labs(
@@ -63,7 +70,7 @@ gr = ggplot(data = df) +
         panel.grid.minor = element_blank(),
         
         axis.text.x = element_text(hjust = 1, vjust = 0.5, family = "Candara", size = 13),
-        axis.text.y = element_text(hjust = 1, vjust = 0.5, family = "Candara", size = 16),
+        axis.text.y = element_text(hjust = 1, vjust = 0.5, family = "Candara", size = 14),
         axis.title.x = element_markdown(family = "Candara", size = 14, margin = margin(t = 14)),
         axis.title.y = element_markdown(family = "Candara", size = 14, margin = margin(r = 14)),
         
@@ -72,6 +79,11 @@ gr = ggplot(data = df) +
         plot.caption = element_markdown(margin = margin(t = 35), size = 11, family = "Candara", hjust = 1),
         
         plot.background = element_rect(fill = "grey93", color = NA)
+    ) +
+    
+    annotate(
+        "text", x = min(df$N), y = -Inf, label = "* NNDSS: National Notifiable Diseases Surveillance System\n     ** NCHS: National Center for Health Statistics",
+        hjust = -0.4, vjust = -1.8, size = 4, family = "Candara", color = "black"
     )
 
 gr
