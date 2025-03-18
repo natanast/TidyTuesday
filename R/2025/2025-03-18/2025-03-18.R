cd7920676814e0e7dbd9e8fab3af4e0b38e7e795
NatAnastas <anastasiadounat@gmail.com>
2025-03-18 21:03:55 +0200

    plot

--- R/2025/2025-03-18/2025-03-18.R
+++ R/2025/2025-03-18/2025-03-18.R
@@ -8,23 +8,36 @@ gc()
 
 library(data.table)
 library(ggplot2)
-library(stringr)
-library(extrafont)
-library(colorspace)
-library(ggtext)
-library(paletteer)
-library(shadowtext)
+library(waffle)
+# library(stringr)
+# library(extrafont)
+# library(colorspace)
+# library(ggtext)
+# library(paletteer)
+# library(shadowtext)
 
 # load data --------
 
-palmtrees <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-03-18/palmtrees.csv')
+palms <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-03-18/palmtrees.csv')
 
 
 # data cleaning ------
 
+# plot ---------
+# Filter data for a specific species (e.g., "Elaeis guineensis")
+brahea_data <- palms %>%
+    filter(acc_genus == "Brahea")  # Replace with the species of interest
 
-# plot --------
-
+# Scatter plot for max_rachis_length_m vs max_petiole_length_m
+ggplot(brahea_data, aes(x = max__rachis__length_m, y = max__petiole_length_m)) +
+    geom_point(aes(color = acc_species), size = 3) +  # Add color by species
+    labs(
+        title = "Comparison of Rachis and Petiole Length for Genus Brahea",
+        x = "Max Rachis Length (m)",
+        y = "Max Petiole Length (m)"
+    ) +
+    theme_minimal() +
+    theme(legend.title = element_blank())  # Remove legend title for clarity
 
 # save ---------
 
