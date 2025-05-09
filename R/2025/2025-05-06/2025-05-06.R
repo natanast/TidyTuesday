49a7be2822816235794cfd2b3a5ccd4085c48b70
NatAnastas <anastasiadounat@gmail.com>
2025-05-09 15:50:48 +0300

    0605

--- R/2025/2025-05-06/2025-05-06.R
+++ R/2025/2025-05-06/2025-05-06.R
@@ -10,9 +10,39 @@ library(data.table)
 library(ggplot2)
 library(extrafont)
 library(ggtext)
+library(waffle)
 
 
 # load data --------
 
-
 nsf_terminations <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-05-06/nsf_terminations.csv')
+
+
+# clean data ------
+
+df = nsf_terminations[, .(directorate)]
+
+
+df = df[!is.na(directorate), .N, by = directorate]
+
+df$directorate <- df$directorate |> str_wrap(width = 15)
+
+# plot ---------
+
+df |> 
+    ggplot(aes(fill = directorate, values = N)) +
+    
+    geom_waffle(color = "white", size = .25, n_rows = 10, flip = TRUE) +
+    
+    facet_wrap(~directorate, nrow = 1, strip.position = "bottom") +
+    
+    theme_minimal() +
+    
+    theme(
+        legend.position = "none",
+        
+        axis.text.x = element_blank(),
+        
+        plot.background = element_rect(fill = "grey93", color = NA),
+        plot.margin = margin(20, 20, 20, 20)
+    )
