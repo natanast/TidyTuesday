b3713a3a8602ed45d3acd3166063c8dd8f969e91
NatAnastas <anastasiadounat@gmail.com>
2025-05-09 15:32:00 +0300

    0605

--- R/2025/2025-05-06/2025-05-06.R
+++ R/2025/2025-05-06/2025-05-06.R
@@ -13,3 +13,6 @@ library(ggtext)
 
 
 # load data --------
+
+
+nsf_terminations <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-05-06/nsf_terminations.csv')
