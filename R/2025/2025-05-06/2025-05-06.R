f4f516ff73898e0491b57c8e6620b3cb698ecb17
NatAnastas <anastasiadounat@gmail.com>
2025-05-09 16:04:08 +0300

    0605

--- R/2025/2025-05-06/2025-05-06.R
+++ R/2025/2025-05-06/2025-05-06.R
@@ -27,17 +27,33 @@ df = df[!is.na(directorate), .N, by = directorate]
 
 df$directorate <- df$directorate |> str_wrap(width = 15)
 
+
+
+# One point per termination
+df_expanded <- df[, .(directorate = rep(directorate, times = N))]
+
+# Create grid layout: each directorate starts at (0,0)
+df_expanded[, id := seq_len(.N), by = directorate]
+df_expanded[, `:=`(
+    x = (id - 1) %% 10,
+    y = (id - 1) %/% 10  # negative y to grow downward
+)]
+
+
+
 # plot ---------
 
-df |> 
-    ggplot(aes(fill = directorate, values = N)) +
+
+df_expanded |> 
     
-    geom_waffle(color = "white", size = .25, n_rows = 10, flip = TRUE) +
+    ggplot(aes(x = x, y = y)) +
+    
+    geom_point(aes(fill = directorate), size = 3, shape = 21, color = "white", stroke = 0.3) +
     
     facet_wrap(~directorate, nrow = 1, strip.position = "bottom") +
-    
-    
-    
+
+    coord_equal() +
+
     theme_minimal(base_family = "Candara") +
     
     theme(
@@ -55,4 +71,8 @@ df |>
 
 
 
+ggsave(
+    plot = gr, filename = "Rplot.png",
+    width = 10, height = 10, units = "in", dpi = 600
+)
 
