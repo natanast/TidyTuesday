d35f2bf180c0bdbae0f651af543f31e659fba5db
NatAnastas <anastasiadounat@gmail.com>
2025-05-16 15:38:06 +0300

    1605

--- R/2025/2025-05-13/2025-05-13.py
+++ R/2025/2025-05-13/2025-05-13.py
@@ -54,45 +54,45 @@ plot2 = (
 
 plot2
 
-g = (
-    ggplot(df_filtered) +
+# g = (
+#     ggplot(df_filtered) +
 
-    aes(x='year', y='Percentage', fill='Population_Type') + 
+#     aes(x='year', y='Percentage', fill='Population_Type') + 
 
-    geom_area(alpha=0.7) +
+#     geom_area(alpha=0.7) +
 
-    facet_wrap('~country') +  
+#     facet_wrap('~country') +  
 
-    scale_fill_manual(values={"Urban": "#6F99AD", "Rural": "#BC3C29"}) + # Custom colors for each quadrant
+#     scale_fill_manual(values={"Urban": "#6F99AD", "Rural": "#BC3C29"}) + # Custom colors for each quadrant
 
-    labs(
-        title = "Urbanization Over Time Across the Globe",
-        subtitle = "Tracking the shift from rural to urban living in selected countries (1960–2023)",
-        caption = "30DayChartChallenge 2025: Day 20 | Source: Urbanization & Climate Metrics Insights (Kaggle) | Graphic: Natasa Anastasiadou",
-        x = '',
-        y = 'Population (%)',
-        fill = 'Type'
-    ) +
+#     labs(
+#         title = "Urbanization Over Time Across the Globe",
+#         subtitle = "Tracking the shift from rural to urban living in selected countries (1960–2023)",
+#         caption = "30DayChartChallenge 2025: Day 20 | Source: Urbanization & Climate Metrics Insights (Kaggle) | Graphic: Natasa Anastasiadou",
+#         x = '',
+#         y = 'Population (%)',
+#         fill = 'Type'
+#     ) +
 
-    theme_minimal() +
+#     theme_minimal() +
 
-    theme(
-        axis_text = element_text(family = 'Candara', size = 8),
-        axis_title = element_text(family = 'Candara', size = 10),
+#     theme(
+#         axis_text = element_text(family = 'Candara', size = 8),
+#         axis_title = element_text(family = 'Candara', size = 10),
         
-        plot_title = element_text(size = 12, weight = 'bold', ha = 'center'),
-        plot_subtitle = element_text(size = 10, ha = 'center'),
-        plot_caption = element_text(size = 6, ha = 'right'),
+#         plot_title = element_text(size = 12, weight = 'bold', ha = 'center'),
+#         plot_subtitle = element_text(size = 10, ha = 'center'),
+#         plot_caption = element_text(size = 6, ha = 'right'),
         
-        panel_grid_major = element_line(color = '#c9c9c9', alpha = 0.75, size = 0.65, linetype="dashed"),
-        plot_background = element_rect(fill = '#f8f8f8', color = '#f8f8f8'),
+#         panel_grid_major = element_line(color = '#c9c9c9', alpha = 0.75, size = 0.65, linetype="dashed"),
+#         plot_background = element_rect(fill = '#f8f8f8', color = '#f8f8f8'),
 
-        legend_title=element_text(size = 8),
-        legend_text=element_text(size = 7),
+#         legend_title=element_text(size = 8),
+#         legend_text=element_text(size = 7),
         
-        figure_size=(10, 6)
-    )
-)
+#         figure_size=(10, 6)
+#     )
+# )
 
 
 #  Save the plot with custom size and resolution
@@ -111,8 +111,8 @@ def classify_event(row):
 
 df['group'] = df.apply(classify_event, axis=1)
 
+df = df[df['year'] > 2012]
 
-from plotnine import *
 
 plot_colored = (
     ggplot(df, aes(x='duration_magnitude_md', y='depth_km', color='group')) +
@@ -127,6 +127,7 @@ plot_colored = (
     labs(title="Depth vs Duration Magnitude by Seismic Event Type",
          x="Duration Magnitude (Md)", y="Depth (km)", color="Event Group") +
     theme_minimal() +
+    facet_wrap('~year') +
     theme(
         axis_text=element_text(family='Candara', size=8),
         axis_title=element_text(family='Candara', size=10),
