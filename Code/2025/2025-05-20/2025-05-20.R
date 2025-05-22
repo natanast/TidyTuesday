ac11eb37c0f7d1b77929f7c676b02890a15041c4
NatAnastas <anastasiadounat@gmail.com>
2025-05-22 12:58:25 +0300

    p

--- Code/2025/2025-05-20/2025-05-20.R
+++ Code/2025/2025-05-20/2025-05-20.R
@@ -16,8 +16,6 @@ library(ggtext)
 # load data --------
 
 water_quality <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-05-20/water_quality.csv')
-# weather <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-05-20/weather.csv')
-
 
 
 # clean data ------
@@ -35,34 +33,34 @@ df$mean_bacteria <- df$mean_bacteria |> round(2)
 
 
 
-# Calculate average mean_bacteria per council
+
 top_councils <- df[, .(avg_bacteria = mean(mean_bacteria, na.rm = TRUE)), by = council]
 
-# Select top N councils, e.g., top 10
 top_councils <- top_councils[order(-avg_bacteria)][1:10, council]
 
-# Filter the original df for only those councils
 df_filtered <- df[council %in% top_councils]
 
 
 
+# plot -------
 
-
-
-# Streamgraph plot
 ggplot(df_filtered, aes(x = year, y = mean_bacteria, fill = council)) +
     
-    geom_stream( extra_span = 0.2 ) +
+    geom_stream( 
+        extra_span = 0.2, 
+        bw = .9
+    ) +
     
     geom_stream(
         extra_span = 0.2, 
         true_range = "none",
+        bw = .9,
         alpha = 0.3
     ) +
     
     scale_fill_viridis_d(option = "turbo", direction = -1) + 
     
-    theme_minimal() +
+    theme_minimal(base_family = "Candara") +
     
     labs(
         title = "Streamgraph of Mean Enterococci Bacteria Levels by Council Over Years",
@@ -72,7 +70,7 @@ ggplot(df_filtered, aes(x = year, y = mean_bacteria, fill = council)) +
     ) +
     
     theme(
-        legend.position = "bottom"
+        legend.position = "right"
     )
 
 
