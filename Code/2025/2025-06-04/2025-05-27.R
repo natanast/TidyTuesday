b2173febb8b15b8fc3e730423cf54b460e7cbb19
NatAnastas <anastasiadounat@gmail.com>
2025-06-07 15:06:56 +0300

    plot

--- Code/2025/2025-06-04/2025-05-27.R
+++ Code/2025/2025-06-04/2025-05-27.R
@@ -24,6 +24,7 @@ gutenberg_metadata <- fread('https://raw.githubusercontent.com/rfordatascience/t
 
 
 # clean data ------
+
 df <- gutenberg_metadata[
     , .(bookshelf = unlist(str_split(gutenberg_bookshelf, "/"))), 
     by = gutenberg_id
@@ -31,76 +32,13 @@ df <- gutenberg_metadata[
 
 df <- df[, .N, by = bookshelf][order(-N)]
 
-df1 <- df[1:100]
+# df1 <- df[1:200]
 
-df1$bookshelf <- df1$bookshelf |> str_remove("^Browsing: ")
-
-
-
-
-gr = ggplot(df_filtered, aes(x = year, y = mean_bacteria, fill = council)) +
-    
-    geom_stream(
-        extra_span = 0.2,
-        bw = .9,
-        lwd = 0.15,
-        color = "grey20"
-    ) +
-
-    geom_stream(
-        extra_span = 0.2, 
-        true_range = "none",
-        bw = .9,
-        alpha = 0.2
-    ) +
-    
-    scale_fill_manual(values = col) +
-    
-    theme_minimal(base_family = "Candara") +
-    
-    labs(
-        title = "Coastal Water Quality Across NSW Councils Over Years",
-        subtitle = "Top 10 councils by mean enterococci bacteria levels by council, measured in CFU per 100mL",
-        caption = "Source: <b>Water Quality at Sydney Beaches data </b> | Graphic: <b>Natasa Anastasiadou</b>", 
-        x = "",
-        y = "Mean Enterococci (CFU/100ml)",
-        fill = "Council"
-    ) +
-    
-    theme(
-        
-        legend.position = "right",
-        legend.key.size = unit(.75, "lines"),      
-        legend.text = element_text(size = 9),      
-        legend.title = element_text(size = 10),
-        
-        axis.text = element_text(size = 11),
-        
-        panel.grid.major = element_line(linewidth = 0.45, color = "grey85"),
-        panel.grid.minor = element_blank(),
-
-        plot.title = element_markdown(size = 16, face = "bold", hjust = 0.5, margin = margin(t = 15, b = 5)),
-        plot.subtitle = element_markdown(size = 14, hjust = 0.5, color = "grey30", margin = margin(t = 2.5, b = 25)),
-        plot.caption = element_markdown(margin = margin(t = 35), size = 9, hjust = .5),
-
-        plot.background = element_rect(fill = "grey93", color = NA),
-        plot.margin = margin(20, 20, 20, 20)
-
-    )
-
-
-ggsave(
-    plot = gr, filename = "plot.png",
-    width = 10, height = 8, units = "in", dpi = 600
-)
-
-
-
-
+df$bookshelf <- df$bookshelf |> str_remove("^Browsing: ")
 
 
 # Process the data 
-df_plot <- df1[, {
+df_plot <- df[, {
     
     dat.egg <- circleProgressiveLayout(N)
     dat.egg <- circleLayoutVertices(dat.egg, npoints = 100)
@@ -114,10 +52,13 @@ df_plot_l <- df_plot |>
     summarise(
         x = (min(x) + max(x)) / 2,
         y = (min(y) + max(y)) / 2
-    )
+    ) |>
+    setDT()
 
 
-df_plot_l$lbl = df_plot_l$bookshelf
+df_plot_l <- df_plot_l[order(-N)][1:20]
+
+df_plot_l$lbl = df_plot_l$bookshelf |> str_wrap(width = 10)
 
 
 
@@ -126,7 +67,7 @@ p <- df_plot |>
     
     ggplot(aes(x, y, group = id)) +
     
-    geom_polygon(aes(fill = bookshelf), color = "grey30", linewidth = .25) +
+    geom_polygon(aes(fill = bookshelf), fill = "#7f9faa", color = "grey30", linewidth = .25) +
     
     geom_shadowtext(
         data = df_plot_l, 
@@ -145,9 +86,9 @@ p <- df_plot |>
     #     
     # ) +
     
-    scale_size_continuous(guide = "none", range = c(5, 9)) +
+    scale_size_continuous(guide = "none", range = c(2, 8)) +
     
-
+    
     # labs(
     #     title = "Expectations vs Performance: Top NCAA Men's March Madness Teams in 2024",
     #     subtitle = "<b> Bubble size</b> represents each team's <b>championship likelihood</b> — bigger size indicated bigger likelihood. <br> <b>Label</b> shows their Performance Against Seed Expectations <b>(PASE)</b> — larger PASE indicates better performance.",
@@ -180,4 +121,14 @@ p <- df_plot |>
     )
 
 p
+
+
+
+ggsave(
+    plot = p, filename = "plot.png",
+    width = 10, height = 8, units = "in", dpi = 600
+)
+
+
+
 
