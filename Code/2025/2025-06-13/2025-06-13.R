bf83e52dfe4ca15eb3231715dff2263e5c74b489
NatAnastas <anastasiadounat@gmail.com>
2025-06-13 12:28:51 +0300

    plot

--- Code/2025/2025-06-13/2025-06-13.R
+++ Code/2025/2025-06-13/2025-06-13.R
@@ -54,27 +54,41 @@ df_top10_F <- df_F[order(-duration)][1:10]
 # Combine both
 df_plot <- rbind(df_top10_F, df_top10_M)
 
+# Set factor levels for plotting
+df_plot[, full_name := factor(full_name, levels = rev(full_name))]
 
 
+# Order alphabetically by last name
+df_plot <- df_plot[order(name_last)]
+
+# Set factor levels in that alphabetical order (so y-axis follows that order)
+df_plot[, full_name := factor(full_name, levels = rev(full_name))]
 
 # 3. Plot
 ggplot(df_plot) +
     
-    geom_segment(aes(x = commission_date, xend = termination_date, y = full_name, yend = full_name), 
-                 color = "#619CFF", linewidth = 1) +
+    geom_segment(aes(x = commission_date, xend = termination_date, y = full_name, 
+                     yend = full_name), 
+                 color = "#619CFF", linewidth = .75) +
     
-    geom_point(aes(x = commission_date, y = full_name), color = "#F8766D", size = 4) +
+    geom_point(aes(x = commission_date, y = full_name), color = "#F8766D", size = 3) +
     
-    geom_point(aes(x = termination_date, y = full_name), color = "#00BFC4", size = 4) +
+    geom_point(aes(x = termination_date, y = full_name), color = "#00BFC4", size = 3) +
     
     scale_x_date(date_breaks = "10 years", date_labels = "%Y") +
     
+    # labs(
+    #     title = "Top 20 Longest-Serving Female Judges in U.S. Federal Courts",
+    #     subtitle = "Commission to Termination Dates",
+    #     x = "Years",
+    #     y = NULL,
+    #     caption = "Source: #TidyTuesday | Data: Federal Judicial Center"
+    # ) +
+    
     labs(
-        title = "Top 20 Longest-Serving Female Judges in U.S. Federal Courts",
-        subtitle = "Commission to Termination Dates",
-        x = "Years",
-        y = NULL,
-        caption = "Source: #TidyTuesday | Data: Federal Judicial Center"
+        title = "Most Popular Bookshelves in the Gutenberg Project",
+        subtitle = "Each bubble represents a bookshelf. <b>Larger </b> bubbles mean more books belong to that category.",
+        caption = "Source: <b> {gutenbergr} R package</b> | Graphic: <b>Natasa Anastasiadou</b>"
     ) +
     
     facet_wrap(vars(gender)) +
@@ -84,64 +98,15 @@ ggplot(df_plot) +
     theme(
         # panel.grid.major.y = element_blank(),
         axis.text.y = element_text(size = 9),
-        
-        plot.title = element_text(face = "bold", size = 16),
-        plot.subtitle = element_text(size = 13, margin = margin(b = 10)),
-        plot.caption = element_text(size = 10, margin = margin(t = 10))
-    )
-
-
-# plot --------
-
-p <- df_plot |>
-    
-    ggplot(aes(x, y, group = id)) +
-    
-    geom_polygon(
-        aes(fill = bookshelf),
-        fill = "#79a3b7", 
-        color = "grey30", 
-        linewidth = .35
-    ) +
-    
-    geom_shadowtext(
-        data = df_plot_l, 
-        aes(x, y, label = lbl, size = N),
-        inherit.aes = FALSE,
-        color = "grey1", 
-        bg.color = "#d9e3f1", 
-        bg.r = .05,
-        family = "Candara"
-    ) +
-
-    scale_size_continuous(guide = "none", range = c(2, 8)) +
-    
-    
-    labs(
-        title = "Most Popular Bookshelves in the Gutenberg Project",
-        subtitle = "Each bubble represents a bookshelf. <b>Larger </b> bubbles mean more books belong to that category.",
-        caption = "Source: <b> {gutenbergr} R package</b> | Graphic: <b>Natasa Anastasiadou</b>"
-    ) +
-
-    theme_minimal(base_family = "Candara") +
-    
-    theme(
-        
         axis.title = element_blank(),
-        axis.text = element_blank(),
         
-        legend.position = "none",
-
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
+        panel.border = element_rect(fill = NA, linewidth = .4),
         
         plot.title = element_markdown(size = 17, face = "bold", hjust = 0.5, margin = margin(t = 5, b = 5)),
         plot.subtitle = element_markdown(size = 14, hjust = 0.5,  color = "grey30"),
         plot.caption  = element_markdown(margin = margin(t = 25), size = 8, hjust = 1),
-
         
         plot.margin = margin(20, 20, 20, 20),
-        
         plot.background = element_rect(fill = "#e4e4e3", color = NA)
         
     )
