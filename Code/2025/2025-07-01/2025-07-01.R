f99ccedd87787288cf6137f98bb2411f41abe5fd
NatAnastas <anastasiadounat@gmail.com>
2025-07-02 16:22:34 +0300

    p

--- Code/2025/2025-07-01/2025-07-01.R
+++ Code/2025/2025-07-01/2025-07-01.R
@@ -50,54 +50,58 @@ ggplot(monthly_gas, aes(x = year_month, y = price, fill = grade)) +
 
 
 
+library(ggridges)
+ggplot(df[fuel == "gasoline" & grade == "regular"], aes(x = price, y = decade, fill = decade)) +
+    geom_density_ridges(scale = 3, alpha = 0.7) +
+    labs(title = "Distribution of Regular Gasoline Prices by Decade")
 
 # Plot -------
-
-p = ggplot(df_plot) +
-    
-    geom_segment(aes(x = commission_date, xend = termination_date, y = full_name, 
-                     yend = full_name), 
-                 color = "grey70", linewidth = .75) +
-    
-    geom_point(aes(x = commission_date, y = full_name), color = "#0072B5", size = 3) +
-    
-    geom_point(aes(x = termination_date, y = full_name), color = "#b24745", size = 3) +
-    
-    scale_x_date(date_breaks = "10 years", date_labels = "%Y") +
-   
-    labs(
-        title = "Most Popular Bookshelves in the Gutenberg Project",
-        subtitle = "Each bubble represents a bookshelf. <b>Larger </b> bubbles mean more books belong to that category.",
-        caption = "Source: <b> {gutenbergr} R package</b> | Graphic: <b>Natasa Anastasiadou</b>"
-    ) +
-    
-    facet_wrap(vars(gender)) +
-    
-    theme_minimal(base_family = "Candara") +
-    
-    theme(
-        # panel.grid.major.y = element_blank(),
-        axis.text.y = element_text(size = 9),
-        axis.text.x = element_text(size = 9, angle = 90),
-        axis.title = element_blank(),
-        
-        
-        panel.grid.major = element_line(linewidth = 0.45, color = "grey85"),
-        panel.grid.minor = element_blank(),
-    
-        panel.border = element_rect(fill = NA, linewidth = .4),
-        
-        plot.title = element_markdown(size = 17, face = "bold", hjust = 0.5, margin = margin(t = 5, b = 5)),
-        plot.subtitle = element_markdown(size = 14, hjust = 0.5,  color = "grey30"),
-        plot.caption  = element_markdown(margin = margin(t = 25), size = 8, hjust = 1),
-        
-        plot.margin = margin(20, 20, 20, 20),
-        plot.background = element_rect(fill = "#e4e4e3", color = NA)
-        
-    )
-
-
-ggsave(
-    plot = p, filename = "plot.png",
-    width = 8.5, height = 9, units = "in", dpi = 600
-)
+# 
+# p = ggplot(df_plot) +
+#     
+#     geom_segment(aes(x = commission_date, xend = termination_date, y = full_name, 
+#                      yend = full_name), 
+#                  color = "grey70", linewidth = .75) +
+#     
+#     geom_point(aes(x = commission_date, y = full_name), color = "#0072B5", size = 3) +
+#     
+#     geom_point(aes(x = termination_date, y = full_name), color = "#b24745", size = 3) +
+#     
+#     scale_x_date(date_breaks = "10 years", date_labels = "%Y") +
+#    
+#     labs(
+#         title = "Most Popular Bookshelves in the Gutenberg Project",
+#         subtitle = "Each bubble represents a bookshelf. <b>Larger </b> bubbles mean more books belong to that category.",
+#         caption = "Source: <b> {gutenbergr} R package</b> | Graphic: <b>Natasa Anastasiadou</b>"
+#     ) +
+#     
+#     facet_wrap(vars(gender)) +
+#     
+#     theme_minimal(base_family = "Candara") +
+#     
+#     theme(
+#         # panel.grid.major.y = element_blank(),
+#         axis.text.y = element_text(size = 9),
+#         axis.text.x = element_text(size = 9, angle = 90),
+#         axis.title = element_blank(),
+#         
+#         
+#         panel.grid.major = element_line(linewidth = 0.45, color = "grey85"),
+#         panel.grid.minor = element_blank(),
+#     
+#         panel.border = element_rect(fill = NA, linewidth = .4),
+#         
+#         plot.title = element_markdown(size = 17, face = "bold", hjust = 0.5, margin = margin(t = 5, b = 5)),
+#         plot.subtitle = element_markdown(size = 14, hjust = 0.5,  color = "grey30"),
+#         plot.caption  = element_markdown(margin = margin(t = 25), size = 8, hjust = 1),
+#         
+#         plot.margin = margin(20, 20, 20, 20),
+#         plot.background = element_rect(fill = "#e4e4e3", color = NA)
+#         
+#     )
+# 
+# 
+# ggsave(
+#     plot = p, filename = "plot.png",
+#     width = 8.5, height = 9, units = "in", dpi = 600
+# )
