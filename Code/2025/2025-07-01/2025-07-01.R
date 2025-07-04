41f46039760a60fcdf8633d96f4098655e72058b
NatAnastas <anastasiadounat@gmail.com>
2025-07-04 16:24:06 +0300

    p

--- Code/2025/2025-07-01/2025-07-01.R
+++ Code/2025/2025-07-01/2025-07-01.R
@@ -11,6 +11,7 @@ library(stringr)
 library(ggplot2)
 
 library(extrafont)
+library(ggtext)
 
 
 # load data ------
@@ -34,16 +35,21 @@ yearly_gas[, decade := paste0(floor(year / 10) * 10, "s")]
 
 # plot --------
 
+col = c('#3a5cbc','#b9b8e7', '#6F99AD', '#FFDC91', '#b24745')
+
+col = c('#3a5cbc','#b9b8e7', '#b24745')
 
 ggplot(yearly_gas, aes(x = factor(year), y = price, fill = grade)) +
     
     geom_bar(stat = "identity", position = "dodge", width = 0.7, alpha = 0.9) +
     
     facet_wrap(~ decade, scales = "free_x") +
+    
+    scale_fill_manual(values = col) +
 
     labs(
         title = "Yearly Average Gasoline Prices",
-        subtitle = "Each bubble represents a bookshelf. <b>Larger </b> bubbles mean more books belong to that category.",
+        subtitle = "",
         caption = "Source: <b> {gutenbergr} R package</b> | Graphic: <b>Natasa Anastasiadou</b>",
         y = "Price per Gallon (USD)",
         fill = "Grade"
@@ -57,21 +63,21 @@ ggplot(yearly_gas, aes(x = factor(year), y = price, fill = grade)) +
         panel.grid.major = element_line(linewidth = 0.45, color = "grey85"),
         panel.grid.minor = element_blank(),
         
-        # plot.title = element_markdown(size = 17, face = "bold", hjust = 0.5, margin = margin(t = 5, b = 5)),
-        # plot.subtitle = element_markdown(size = 14, hjust = 0.5,  color = "grey30"),
-        # plot.caption  = element_markdown(margin = margin(t = 25), size = 8, hjust = 1),
+        plot.title = element_markdown(size = 17, face = "bold", hjust = 0.5, margin = margin(t = 5, b = 5)),
+        plot.subtitle = element_markdown(size = 14, hjust = 0.5,  color = "grey30"),
+        plot.caption  = element_markdown(margin = margin(t = 25), size = 8, hjust = 1),
         
-        plot.background = element_rect(fill = "#e4e4e3", color = NA),
+        plot.background = element_rect(fill = "grey90", color = NA),
         plot.margin = margin(20, 20, 20, 20)
     )
 
 
 
-# 
-# ggsave(
-#     plot = p, filename = "plot.png",
-#     width = 8.5, height = 9, units = "in", dpi = 600
-# )
+
+ggsave(
+    plot = p, filename = "plot.png",
+    width = 8.5, height = 9, units = "in", dpi = 600
+)
 
 
 
