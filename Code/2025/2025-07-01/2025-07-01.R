6c4aa7b72c2aa57523ab5f957885c88a94e42bc1
NatAnastas <anastasiadounat@gmail.com>
2025-07-04 16:12:39 +0300

    p

--- Code/2025/2025-07-01/2025-07-01.R
+++ Code/2025/2025-07-01/2025-07-01.R
@@ -10,6 +10,8 @@ library(data.table)
 library(stringr)
 library(ggplot2)
 
+library(extrafont)
+
 
 # load data ------
 
@@ -44,16 +46,39 @@ yearly_gas[, decade := paste0(floor(year / 10) * 10, "s")]
 
 
 ggplot(yearly_gas, aes(x = factor(year), y = price, fill = grade)) +
-    geom_col(position = "dodge", alpha = 0.8, width = .75) +
+    
+    geom_bar(stat = "identity", position = "dodge", width = 0.7, alpha = 0.9) +
+
+    # geom_col(position = "dodge", alpha = 0.8, width = .75) +
+    
     facet_wrap(~ decade, scales = "free_x") +
+
+    
     labs(
         title = "Yearly Average Gasoline Prices",
-        x = "Year",
+        subtitle = "Each bubble represents a bookshelf. <b>Larger </b> bubbles mean more books belong to that category.",
+        caption = "Source: <b> {gutenbergr} R package</b> | Graphic: <b>Natasa Anastasiadou</b>",
         y = "Price per Gallon (USD)",
         fill = "Grade"
     ) +
-    theme_minimal() +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+    
+    theme_minimal(base_family = "Calibri") +
+    
+    theme(
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        
+        panel.grid.major = element_line(linewidth = 0.45, color = "grey85"),
+        panel.grid.minor = element_blank(),
+        
+        # plot.title = element_markdown(size = 17, face = "bold", hjust = 0.5, margin = margin(t = 5, b = 5)),
+        # plot.subtitle = element_markdown(size = 14, hjust = 0.5,  color = "grey30"),
+        # plot.caption  = element_markdown(margin = margin(t = 25), size = 8, hjust = 1),
+        
+        plot.background = element_rect(fill = "#e4e4e3", color = NA),
+        plot.margin = margin(20, 20, 20, 20)
+    )
+
+
 
 
 # Plot -------
@@ -70,11 +95,7 @@ ggplot(yearly_gas, aes(x = factor(year), y = price, fill = grade)) +
 #     
 #     scale_x_date(date_breaks = "10 years", date_labels = "%Y") +
 #    
-#     labs(
-#         title = "Most Popular Bookshelves in the Gutenberg Project",
-#         subtitle = "Each bubble represents a bookshelf. <b>Larger </b> bubbles mean more books belong to that category.",
-#         caption = "Source: <b> {gutenbergr} R package</b> | Graphic: <b>Natasa Anastasiadou</b>"
-#     ) +
+
 #     
 #     facet_wrap(vars(gender)) +
 #     
