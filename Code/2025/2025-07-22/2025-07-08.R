82e488f3fccc8ee88c2e5457bb5b8560f4512c0d
Natasa Anastasiadou <141711668+natanast@users.noreply.github.com>
2025-07-25 19:20:27 +0300

    Update 2025-07-08.R

--- Code/2025/2025-07-22/2025-07-08.R
+++ Code/2025/2025-07-22/2025-07-08.R
@@ -16,9 +16,6 @@ library(ggtext)
 
 # load data ------
 
-# answers <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-07-08/answers.csv')
-color_ranks <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-07-08/color_ranks.csv')
-# users <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-07-08/users.csv')
 
 
 # clean data ------
