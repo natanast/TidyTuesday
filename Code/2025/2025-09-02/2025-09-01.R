b1370a2ca1d4a9cf838ab7cd3ad947f3457a16ca
NatAnastas <anastasiadounat@gmail.com>
2025-09-05 10:29:38 +0300

    p

--- Code/2025/2025-09-02/2025-09-01.R
+++ Code/2025/2025-09-02/2025-09-01.R
@@ -11,6 +11,7 @@ library(ggplot2)
 library(stringr)
 library(extrafont)
 library(colorspace)
+library(ggtext)
 
 
 # load data ------
@@ -41,7 +42,7 @@ gr = ggplot() +
     
     
     geom_polygon(data = aus_map, aes(x = long, y = lat, group = group),
-                 fill = "grey90", color = "white") +
+                 fill = "grey85", color = "grey75") +
     
     geom_point(data = frogs_merged, 
                aes(x = decimalLongitude, y = decimalLatitude),
@@ -55,22 +56,39 @@ gr = ggplot() +
     
     labs(
         title = "FrogID observations across Australia",
-        subtitle = "Top 5 increases and decreases in view counts between the first and last reporting periods",
+        # subtitle = "Top 5 increases and decreases in view counts between the first and last reporting periods",
         caption = "Source: <b>FrogID data</b> | Graphic: <b>Natasa Anastasiadou</b>",
+        x = "Longitude",
+        y = "Latitude"
     ) +
     
     theme(
         
-        panel.grid.major = element_line(linewidth = 0.45, color = "grey95", linetype = "dashed"),
+        
+        plot.title = element_markdown(size = 16, face = "bold", hjust = .5, margin = margin(b = 5, t = 5)),
+        plot.subtitle = element_markdown(size = 14, hjust = 0.6, color = "grey30", margin = margin(b = 25, t = 5)),
+        plot.caption = element_markdown(margin = margin(t = 35), size = 9, hjust = 1.85),
+        
+        panel.grid.major = element_line(linewidth = 0.25, color = "grey80", linetype = "dashed"),
         panel.grid.minor = element_blank(),
+        
+        panel.border = element_rect(color = "grey75", fill = NA, linewidth = .4),
         
         plot.margin = margin(20, 20, 20, 20),
         
-        plot.background = element_rect(fill = "grey94", color = NA)
+        plot.background = element_rect(fill = "#e4e4e3", color = NA)
     )
 
 
 gr
+
+
+ggsave(
+    plot = gr, filename = "plot.png",
+    width = 10, height = 10, units = "in", dpi = 600
+)
+
+
 
 # plot --------
 
@@ -174,7 +192,7 @@ gr
 
 ggsave(
     plot = gr, filename = "plot.png",
-    width = 9, height = 9, units = "in", dpi = 600
+    width = 10, height = 10, units = "in", dpi = 600
 )
 
 
