0bf01ab71d07c57a20372eb8c0e9314a0b202760
NatAnastas <anastasiadounat@gmail.com>
2025-09-05 11:38:25 +0300

    p

--- Code/2025/2025-09-02/2025-09-01.R
+++ Code/2025/2025-09-02/2025-09-01.R
@@ -42,11 +42,18 @@ gr = ggplot() +
     
     
     geom_polygon(data = aus_map, aes(x = long, y = lat, group = group),
-                 fill = "grey", color = "grey75", alpha = 0.75) +
+                 fill = "#fffaf9", color = "grey70", alpha = 0.65) +
     
-    geom_point(data = frogs_merged, 
-               aes(x = decimalLongitude, y = decimalLatitude),
-               alpha = 0.3, size = 0.5, color = "#6F99AD") +
+    geom_point(
+        data = frogs_merged, 
+        aes(x = decimalLongitude, y = decimalLatitude),
+        alpha = 0.5, 
+        size = 1, 
+        stroke = .25,
+        color = "#6F99AD" |> darken(.25), 
+        fill = "#6F99AD" |> lighten(.25),
+        shape = 21
+    ) +
     
     coord_fixed(1.3) +
     
@@ -55,8 +62,8 @@ gr = ggplot() +
     theme_minimal(base_family = "Candara") +
     
     labs(
-        title = "FrogID observations across Australia",
-        # subtitle = "Top 5 increases and decreases in view counts between the first and last reporting periods",
+        title = "Seasonal Distribution of Frog Observations in Australia",
+        subtitle = "FrogID citizen science recordings reveal the distribution of frogs across Australia throughout the year.",
         caption = "Source: <b>FrogID data</b> | Graphic: <b>Natasa Anastasiadou</b>",
         x = "Longitude",
         y = "Latitude"
@@ -67,18 +74,17 @@ gr = ggplot() +
         
         plot.title = element_markdown(size = 16, face = "bold", hjust = .5, margin = margin(b = 5, t = 5)),
         plot.subtitle = element_markdown(size = 14, hjust = 0.6, color = "grey30", margin = margin(b = 25, t = 5)),
-        plot.caption = element_markdown(margin = margin(t = 35), size = 9, hjust = 1.85),
+        plot.caption = element_markdown(margin = margin(t = 35), size = 9, hjust = 1.5),
         
         panel.grid.major = element_line(linewidth = 0.25, color = "grey80", linetype = "dashed"),
         panel.grid.minor = element_blank(),
         
-        panel.border = element_rect(color = "grey75", fill = NA, linewidth = .4),
+        panel.border = element_rect(color = "grey70", fill = NA, linewidth = .4),
         
         plot.margin = margin(20, 20, 20, 20),
         
-        plot.background = element_rect(fill = "#e4e4e3", color = NA)
+        plot.background = element_rect(fill = "#e8e8e7", color = NA)
     )
-
 
 gr
 
