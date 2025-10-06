c92526a077c7cde84ca2350e522a75f5474a10ad
NatAnastas <anastasiadounat@gmail.com>
2025-10-06 14:27:09 +0300

    plot

--- Code/2025/2025-10-07/2025-10-07.R
+++ Code/2025/2025-10-07/2025-10-07.R
@@ -11,7 +11,7 @@ library(ggplot2)
 library(stringr)
 library(extrafont)
 library(colorspace)
-
+library(ggtext)
 
 # load data ------
 
@@ -31,7 +31,7 @@ finalfour_long <- df1[, .(
 # Separate Title years
 titles_long <- df1[, .(
     Year = unlist(str_split(Years_of_Titles_Won, ",\\s*")),
-    Status = "Champion"
+    Status = "Championship"
 ), by = Team]
 
 
@@ -46,80 +46,77 @@ df_long <- df_long[Year != ""]
 df_long[, Year := as.integer(Year)]
 
 
-
-
 # make sure Team is a factor and ordered by number of Titles (optional)
-team_order <- df_long[Status == "Champion", .N, by = Team][order(-N)]$Team
+team_order <- df_long[Status == "Championship", .N, by = Team][order(-N)]$Team
 team_order <- unique(c(team_order, df_long$Team))
 df_long[, Team := factor(Team, levels = rev(team_order))]  
 
 
+
 # plot ------ 
-ggplot(df_long, aes(x = Year, y = Team, color = Status)) +
-    geom_point(size = 3) +
-    scale_color_manual(values = c("Final Four" = "grey70", "Champion" = "#D94E67")) +
-    labs(title = "Euroleague: Final Four appearances and Titles",
-         x = "Year", y = NULL, color = "") +
-    theme_minimal() +
-    theme(
-        axis.text.y = element_text(size = 9),
-        plot.title = element_text(hjust = 0.5, face = "bold"),
-        legend.position = "top"
-    )
 
+col = c("#426b7b", "#9fc6d9")
 
-# plot -------
 
 gr = ggplot() +
     
-    
-    geom_polygon(data = aus_map, aes(x = long, y = lat, group = group),
-                 fill = "#fffaf9", color = "grey70", alpha = 0.65) +
-    
     geom_point(
-        data = frogs_merged, 
-        aes(x = decimalLongitude, y = decimalLatitude),
-        alpha = 0.5, 
-        size = 1, 
-        stroke = .25,
-        color = "#6F99AD" |> darken(.25), 
-        fill = "#6F99AD" |> lighten(.25),
+        data = df_long, 
+        aes(x = Year, y = Team, color = Status, fill = Status), 
+        size = 4.5, 
+        stroke = .35,
+        # alpha = 0.85,
         shape = 21
+        
     ) +
     
-    coord_fixed(1.3) +
+    scale_color_manual(values = col |> darken(.25) ) +
+    scale_fill_manual(values = col |> lighten(.25) ) +
     
-    facet_wrap(~season) +
+    # Make x-axis continuous
+    scale_x_continuous(
+        limits = c(1988, 2025),          # set range
+        breaks = seq(1988, 2025, by = 2) # put a tick every 2 years
+    ) +
+    
+    labs(
+        title = "Road to Glory: EuroLeague Teams’ Final Four and Titles (1988–2025)",
+        subtitle = "<b><span style='color: #426b7b; font-weight: bold;'>Darker</span></b> dots indicate <b><span style='color: #426b7b; font-weight: bold;'>Championship victories</span></b>, 
+                    while <b><span style='color: #81a7ba; font-weight: bold;'>lighter</span></b> dots indicate  <b><span style='color: #81a7ba; font-weight: bold;'>Final Four appearances</span></b>.",
+        caption = "Source: <b>Euroleague dataset</b> | Graphic: <b>Natasa Anastasiadou</b>",
+        x = "Year",
+        y = "",
+        fill = ""
+    ) +
+    
+    guides(color = "none") +
     
     theme_minimal(base_family = "Candara") +
     
-    labs(
-        title = "Seasonal Distribution of Frog Observations in Australia",
-        subtitle = "FrogID data provide a continent-wide view of frog activity across time and space.",
-        caption = "Source: <b>FrogID data</b> | Graphic: <b>Natasa Anastasiadou</b>",
-        x = "Longitude",
-        y = "Latitude"
-    ) +
-    
     theme(
-        plot.title = element_markdown(size = 16, face = "bold", hjust = .5, margin = margin(b = 5, t = 5)),
-        plot.subtitle = element_markdown(size = 14, hjust = 0.6, color = "grey30", margin = margin(b = 25, t = 5)),
-        plot.caption = element_markdown(margin = margin(t = 35), size = 9, hjust = 1.5),
+        plot.title = element_markdown(size = 15, face = "bold", hjust = .4, margin = margin(b = 5, t = 5)),
+        plot.subtitle = element_markdown(size = 13, hjust = .1, color = "grey30", margin = margin(b = 5, t = 5)),
+        plot.caption = element_markdown(margin = margin(t = 15), size = 8, hjust = 1),
         
         panel.grid.major = element_line(linewidth = 0.25, color = "grey80", linetype = "dashed"),
         panel.grid.minor = element_blank(),
         
-        panel.border = element_rect(color = "grey70", fill = NA, linewidth = .4),
+        axis.title.x = element_text(size = 11, margin = margin(t = 15)),
+        
+        axis.text = element_text(size = 10),
+        
+        legend.position = "top",
         
         plot.margin = margin(20, 20, 20, 20),
         
         plot.background = element_rect(fill = "#e8e8e7", color = NA)
     )
 
+gr
 
 ggsave(
     plot = gr, filename = "plot.png",
-    width = 10, height = 10, units = "in", dpi = 600
+    width = 10, height = 8, units = "in", dpi = 600
 )
 
 
