95ffc4629c1c9d7dc9a844711a09b94dba58e66e
NatAnastas <anastasiadounat@gmail.com>
2026-01-28 20:13:09 +0200

    p

--- Code/2026/2026-01-27/2026-01-27.R
+++ Code/2026/2026-01-27/2026-01-27.R
@@ -9,34 +9,37 @@ gc()
 library(data.table)
 library(ggplot2)
 library(stringr)
-# library(extrafont)
+library(extrafont)
 # library(colorspace)
 library(ggtext)
+library(scales)
 
 
 # load data ------
 
 companies <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-01-27/companies.csv')
-legal_nature <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-01-27/legal_nature.csv')
-qualifications <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-01-27/qualifications.csv')
-size <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-01-27/size.csv')
+# legal_nature <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-01-27/legal_nature.csv')
+# qualifications <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-01-27/qualifications.csv')
+# size <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-01-27/size.csv')
 
 
 # clean data ------
 
 d <- companies[, .(legal_nature, capital_stock, company_size)]
 
-d1 <- d[, .N, by = .(company_size, legal_nature, capital_stock)]
+d1 <- d[, .(median_capital = median(capital_stock, na.rm = TRUE), n_companies = .N), by = .(company_size, legal_nature)]
+
+d1 <- d1[n_companies > 10]
 
 
-plot_data <- d[, .(
-    median_capital = median(capital_stock, na.rm = TRUE),
-    n_companies = .N    # .N is a special symbol in data.table for "count"
-), by = .(company_size, legal_nature)][n_companies > 10]
+d1$legal_nature <- factor(d1$legal_nature, levels = sort(unique(d1$legal_nature), decreasing = TRUE))
+
+col = c('#3a5cbc','#6F99AD','#F39B7F')
+
 
 # plot -------
 
-ggplot(plot_data) +
+gr = ggplot(d1) +
     
     geom_point(
         aes(x = median_capital, y = legal_nature, size = n_companies, fill = company_size),
@@ -44,11 +47,13 @@ ggplot(plot_data) +
     ) +
     
     scale_x_log10(
-        
         labels = label_number(scale_cut = cut_short_scale(), prefix = "")
     ) +
-    
-    scale_size_continuous(range = c(3, 7)) +
+
+    scale_size_continuous(
+        range = c(3, 7),
+        name = "No. of Companies"
+    ) +
     
     facet_grid(
         rows = vars(company_size),
@@ -56,15 +61,20 @@ ggplot(plot_data) +
         space = "free_y"
     ) +
     
-    # labs(
-    #     title = "Seasonal Distribution of Frog Observations in Australia",
-    #     subtitle = "FrogID data provide a continent-wide view of frog activity across time and space.",
-    #     caption = "Source: <b>FrogID data</b> | Graphic: <b>Natasa Anastasiadou</b>",
-    #     x = "Longitude",
-    #     y = "Latitude"
-    # ) +
+    scale_fill_manual(
+        values = col,
+        guide = "none"
+    ) +
     
-    theme_minimal() +
+    labs(
+        title = "Median Capital Stock of Brazilian Companies",
+        subtitle = "Median capital (BRL) by legal nature and size category.",
+        caption = "Source: <b>Brazilian Companies data</b> | Graphic: <b>Natasa Anastasiadou</b>",
+        x = "Media Capital Stock",
+        y = NULL
+    ) +
+
+    theme_minimal(base_family = "Candara") +
     
     theme(
         
@@ -72,13 +82,10 @@ ggplot(plot_data) +
         plot.subtitle = element_markdown(size = 14, hjust = 0.6, color = "grey30", margin = margin(b = 25, t = 5)),
         plot.caption = element_markdown(margin = margin(t = 35), size = 9, hjust = 1.5),
         
-        axis.title.y = element_blank(),
-        
         panel.grid.major = element_line(linewidth = 0.25, color = "grey80", linetype = "dashed"),
         panel.grid.minor = element_blank(),
         
-        panel.border = element_rect(fill = NA, linewidth = .4),
-        axis.ticks = element_line(linewidth = .4),
+        panel.border = element_rect(color = "grey40", fill = NA, linewidth = .4),
         
         plot.background = element_rect(fill = "#fffaf9", color = NA),
         
@@ -87,56 +94,6 @@ ggplot(plot_data) +
 
     
     
-    
-    
-
-ggplot(d, aes(x = company_size, y = capital_stock)) +
-    geom_violin() +
-
-    geom_polygon(data = aus_map, aes(x = long, y = lat, group = group),
-                 fill = "#fffaf9", color = "grey70", alpha = 0.65) +
-    
-    geom_point(
-        data = frogs_merged, 
-        aes(x = decimalLongitude, y = decimalLatitude),
-        alpha = 0.5, 
-        size = 1, 
-        stroke = .25,
-        color = "#6F99AD" |> darken(.25), 
-        fill = "#6F99AD" |> lighten(.25),
-        shape = 21
-    ) +
-    
-    coord_fixed(1.3) +
-    
-    facet_wrap(~season) +
-    
-    theme_minimal(base_family = "Candara") +
-    
-    labs(
-        title = "Seasonal Distribution of Frog Observations in Australia",
-        subtitle = "FrogID data provide a continent-wide view of frog activity across time and space.",
-        caption = "Source: <b>FrogID data</b> | Graphic: <b>Natasa Anastasiadou</b>",
-        x = "Longitude",
-        y = "Latitude"
-    ) +
-    
-    theme(
-        
-        plot.title = element_markdown(size = 16, face = "bold", hjust = .5, margin = margin(b = 5, t = 5)),
-        plot.subtitle = element_markdown(size = 14, hjust = 0.6, color = "grey30", margin = margin(b = 25, t = 5)),
-        plot.caption = element_markdown(margin = margin(t = 35), size = 9, hjust = 1.5),
-        
-        panel.grid.major = element_line(linewidth = 0.25, color = "grey80", linetype = "dashed"),
-        panel.grid.minor = element_blank(),
-        
-        panel.border = element_rect(color = "grey70", fill = NA, linewidth = .4),
-        
-        plot.margin = margin(20, 20, 20, 20),
-        
-        plot.background = element_rect(fill = "#e8e8e7", color = NA)
-    )
-
 gr
 
 
